99f41166fb54cf0fe14d9c543d1039087a7d3e90
Matthew Webb <Matthew.Webb12@udal.nhs.uk>
2026-01-14 09:29:28 +0000

    project 1 updated - rendering complete

--- pandas_download.R
+++ pandas_download.R
@@ -6,5 +6,4 @@ use_condaenv("r-reticulate", required = TRUE)
 # Install your Python packages into that environment
 py_install(c("pandas", "openpyxl", "seaborn", "matplotlib"), pip = TRUE)
 
-
-
+# rmarkdown::render_site()
