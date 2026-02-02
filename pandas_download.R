93b32e241daafc4923f86bd5303bae3fecf41210
Matthew Webb <Matthew.Webb12@udal.nhs.uk>
2026-02-02 15:36:24 +0000

    added output to site

--- pandas_download.R
+++ pandas_download.R
@@ -6,4 +6,4 @@ use_condaenv("r-reticulate", required = TRUE)
 # Install your Python packages into that environment
 py_install(c("pandas", "openpyxl", "seaborn", "matplotlib", "scikit-learn", "jinja2", "ipython", "tabulate"), pip = TRUE)
 
-# rmarkdown::render_site()
+  # rmarkdown::render_site()
